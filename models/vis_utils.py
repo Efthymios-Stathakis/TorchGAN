b3ff8704dcb63595c01edbaeb028112d2f3e22dd
Efthymios-Stathakis <efstatha@hotmail.com>
2024-12-20 11:37:36 +0100

    Added option to save figure

--- models/vis_utils.py
+++ models/vis_utils.py
@@ -1,8 +1,13 @@
 from torchvision.utils import make_grid
 import matplotlib.pyplot as plt
 
-def show_img_batch(img_batch, n_images=32, nrow=8, size=(1,28,28)):
+def show_img_batch(img_batch, n_images=32, nrow=8, size=(1,28,28), save_path=None):
     img_batch = img_batch.detach().cpu().view(-1, *size)
     img_grid = make_grid(img_batch[:n_images], nrow=nrow)
-    plt.imshow(img_grid.permute(1,2,0).squeeze())
-    plt.show()
+    if save_path:
+        plt.ioff()
+        plt.imshow(img_grid.permute(1,2,0).squeeze())
+        plt.savefig(save_path)
+    else:
+        plt.imshow(img_grid.permute(1,2,0).squeeze())
+        plt.show()
