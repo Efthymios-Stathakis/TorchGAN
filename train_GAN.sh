ca83c419d07dcca7b3c1348ccd2361595426726e
Efthymios-Stathakis <efstatha@hotmail.com>
2024-12-22 09:55:17 +0100

    Added comments in train_GAN.sh

--- train_GAN.sh
+++ train_GAN.sh
@@ -3,14 +3,14 @@
 # Save this script as run_dcgan.sh and give it execute permissions
 # Usage: ./run_dcgan.sh [n_epochs] [z_dim] [batch_size] [hidden_dim] [lr] [beta_1] [beta_2]
 
-n_epochs=${1:-50}
+n_epochs=${1:-100} # 50, 100, 200
 z_dim=${2:-128}
 batch_size=${3:-128}
 hidden_dim=${4:-64}
 lr=${5:-0.0002}
 beta_1=${6:-0.5}
 beta_2=${7:-0.999}
-model_type=${8:-DCGAN}
+model_type=${8:-ConditionalGAN} # DCGAN, ACGAN, ConditionalGAN, WGANGP
 dataset=${8:-mnist}
 
 python3 -m models.$model_type.train --n_epochs $n_epochs \
